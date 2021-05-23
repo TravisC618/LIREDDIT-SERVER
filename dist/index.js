4bd32abb657346e98faee210d8d79da6855329a6
travisC <travis_zg@outlook.com>
2021-05-24 09:17:37 +1000

    break point

--- dist/index.js
+++ dist/index.js
@@ -26,6 +26,7 @@ const redis_1 = __importDefault(require("redis"));
 const express_session_1 = __importDefault(require("express-session"));
 const connect_redis_1 = __importDefault(require("connect-redis"));
 const hello_1 = require("./resolvers/hello");
+const cors_1 = __importDefault(require("cors"));
 const PORT = process.env.PORT || 4000;
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
@@ -33,6 +34,10 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const app = express_1.default();
     const RedisStore = connect_redis_1.default(express_session_1.default);
     const redisClient = redis_1.default.createClient();
+    app.use(cors_1.default({
+        origin: 'http://localhost:3000',
+        credentials: true
+    }));
     app.use(express_session_1.default({
         name: 'qid',
         store: new RedisStore({ client: redisClient, disableTouch: true }),
@@ -53,7 +58,7 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
         }),
         context: ({ req, res }) => ({ em: orm.em, req, res })
     });
-    apolloServer.applyMiddleware({ app });
+    apolloServer.applyMiddleware({ app, cors: false });
     app.listen(PORT, () => {
         console.log(`server started on PORT: ${PORT}`);
     });
